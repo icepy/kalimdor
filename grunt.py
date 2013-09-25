286008fb5392f73cbb410440dbbccd2c37267044
xiangwenwe <xiangwenwe@foxmail.com>
2013-09-25 15:07:09 +0800

    配置自动化工具
    配置自动化工具

---
+++ grunt.py
@@ -0,0 +1,33 @@
+﻿#前端自动化工具（功能）@代表，已经实现的功能
+#	@部署环境，合并文件。
+#	未来增加：部署环境，压缩。	
+
+import string
+import sys
+import libs.config
+
+x = libs.config.file_config
+def gruntFile(x):
+	y = {}
+	_newFile = {};
+	for _key,_value in x.items():
+		_newFile[_key] = open('new/'+_key+'.js','w',encoding = 'utf-8')
+		y[_key] = []
+		for _file in _value:
+			print('合并的文件：'+_file)
+			_open = open(_file,'r',encoding='utf-8');
+			_content = _open.read()
+			y[_key].append(_content) 
+			_open.close()
+			pass
+		pass
+	for _k,_v in y.items():
+		_f = _newFile[_k]
+		for _Content in _v:
+			_f.write(_Content)
+			pass
+		print(_k +'.js------文件合并完成完成')
+		_f.close()
+		pass
+	pass
+gruntFile(x)
---
+++ libs/config.py
@@ -0,0 +1,11 @@
+﻿file_config = {
+	"libs":(
+		"libs/jquery.js",
+		"libs/jquery.tmpl.js"
+	),
+	"plug":(
+		"libs/seeui.js",
+		"libs/plug/seeui.dialog.js",
+		"libs/plug/seeui.roll.js"
+	)
+} 
